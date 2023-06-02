f18949d087b9b8a23b52cc5aca9d2f33e2d4f718
Auzmit <atrudkov@mail.ru>
2023-06-02 22:13:10 +0300

    more explanation

--- js/greenColorAndDate.js
+++ js/greenColorAndDate.js
@@ -1,8 +1,19 @@
 let date = new Date();
+
+/* Ищем в документе элемент с классом ".square"
+   и заводим его в переменную */
 let divSquare = document.querySelector('.square');
 
+/* Создаём функцию, которая выполнится по клику */
 divSquare.onclick = function() {
+
+  /* Убираем класс "red_background" */
   divSquare.classList.remove('red_background');
-  divSquare.innerHTML = "<h3>" + date + "</h3>";
+  
+  /* Добавляем класс "green_background" */
   divSquare.classList.add('green_background');
+
+  /* Создаём заголовок третьей величины с текущей датой */
+  divSquare.innerHTML = "<h3>" + date + "</h3>";
+
 };
