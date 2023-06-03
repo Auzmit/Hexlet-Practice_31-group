82277c8f6257afa619f189012f7af68f659e621a
Auzmit <atrudkov@mail.ru>
2023-06-04 00:07:46 +0300

    css & js

--- js/greenColorAndDate.js
+++ js/greenColorAndDate.js
@@ -1,19 +1,35 @@
 let date = new Date();
 
-/* Ищем в документе элемент с классом ".square"
-   и заводим его в переменную */
-let divSquare = document.querySelector('.square');
+/* Создайм константу h3, создающую заголовок третьего уровня */
+const h3 = document.createElement('h3');
+
+/* Ищем в документе элемент с классом ".square" и заводим его в переменную */
+let divSquare = document.querySelector('.div_square');
 
 /* Создаём функцию, которая выполнится по клику */
 divSquare.onclick = function() {
 
-  /* Убираем класс "red_background" */
-  divSquare.classList.remove('red_background');
-  
-  /* Добавляем класс "green_background" */
-  divSquare.classList.add('green_background');
+  /* Если divSquare имеет класс red_background, то... */
+  if (this.classList.contains("red-background")) {
 
-  /* Создаём заголовок третьей величины с текущей датой */
-  divSquare.innerHTML = "<h3 class='h3_date'>" + date + "</h3>";
-  
+    /* Удаляем у него класс red_background */
+    this.classList.remove("red-background");
+
+    /* Добавляем ему класс green_background */
+    this.classList.add("green-background");
+
+    /* Добавляем в divSquare элемент h3, в который добаляем текущую дату */
+    this.appendChild(h3);
+    h3.innerHTML = date;
+    h3.classList.add('h3_date');
+    // console.log(divSquare.children);
+  } else {
+
+    /* ну, тут аналогично */
+    this.classList.remove("green-background");
+    this.classList.add("red-background");
+    this.removeChild(h3);
+
+    // divSquare.removeChild(h3);
+  }
 };
