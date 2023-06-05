76018302b76d0bb3023258c6f56e4254874fc745
Auzmit <atrudkov@mail.ru>
2023-06-05 21:01:53 +0300

    eslint fix

--- js/greenColorAndDate.js
+++ js/greenColorAndDate.js
@@ -2,29 +2,26 @@
 const h1 = document.createElement('h1');
 
 /* Ищем в документе элемент с классом ".square" и заводим его в переменную */
-let divSquare = document.querySelector('.div_square');
+const divSquare = document.querySelector('.div_square');
 
 /* Создаём функцию, которая выполнится по клику */
-divSquare.onclick = function() {
-
+divSquare.onclick = function () {
   /* Если divSquare имеет класс red_background, то... */
-  if (this.classList.contains("red-background")) {
-
+  if (this.classList.contains('red-background')) {
     /* Удаляем у него класс red_background */
-    this.classList.remove("red-background");
+    this.classList.remove('red-background');
 
     /* Добавляем ему класс green_background */
-    this.classList.add("green-background");
+    this.classList.add('green-background');
 
     /* Добавляем в divSquare элемент h1, в который добаляем текущую дату */
     this.appendChild(h1);
     h1.innerHTML = new Date();
     h1.classList.add('h1_date');
   } else {
-
     /* ну, тут аналогично */
-    this.classList.remove("green-background");
-    this.classList.add("red-background");
+    this.classList.remove('green-background');
+    this.classList.add('red-background');
     this.removeChild(h1);
   }
 };
