9a136ba120b2f0fd2fcd1f09ddacf345546086c1
IzarlyShark <115780892+IzarlyShark@users.noreply.github.com>
2023-06-07 00:21:16 +0300

    кнопочка домой без стилей на игр стр

--- js/game.js
+++ js/game.js
@@ -1,3 +1,4 @@
+// наш объет: темы - ключ, значение - список слов
 const wordLists = {
   animals: ['аллигатор', 'антилопа', 'бабуин', 'барсук', 'медведь', 'кошка', 'хамелеон', 'гепард', 'шиншилла', 'кобра', 'койот', 'выхухоль', 'жираф', 'гекон', 'свинья'],
   fish: ['креветка', 'мидия', 'краб', 'осьминог', 'кальмар', 'устрица', 'скумбрия', 'лосось', 'тунец', 'корюшка', 'дельфин', 'акула', 'пиранья', 'планктон', 'коралл'],
@@ -8,28 +9,36 @@ const wordLists = {
 
 };
 
+//максимальное кол-во попыток
 const maxAttempts = 6;
-const alphabet = 'абвгдежзийклмнопрстуфхцчшщъыьэюя';
+//наша строка - алфавит
+const alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
+//разбиваем строку алфавита на массив букв
 const alphabetLetters = alphabet.split('');
 
+//рандомайзер случайного слова из списка Math.trunc откидывает дробную часть, list это список слов по какойто конкр теме
 function pickRandomWord(list) {
   const index = Math.trunc(Math.random() * list.length);
   return list[index];
 }
 
+//объект игры, который мы динамично наполняем данными
 const gameState = {
   topic: null,
   word: null,
   openedLetters: [],
   attempts: 0,
+  //функция инициализации мы берем topic из ссылки по параметру (по теме), припарируем ссылку, забираем параметр - который идентичен ключам в объекте
   init() {
     this.topic = new URLSearchParams(window.location.search).get('topic');
     this.word = pickRandomWord(wordLists[this.topic]);
   },
+  //добавляем в массив выбранную букву 
   openLetter(letter) {
     this.openedLetters.push(letter);
-
+  //буква найдена в слове есть совпадение
     const isLetterFound = this.getWordLetters().includes(letter);
+    //не найдено совпадение, счетчик увеличивается на 1, если это последняя попытка, то конец игры
     if (!isLetterFound) {
       this.attempts += 1;
       if (this.isGameOver()) {
@@ -37,76 +46,96 @@ const gameState = {
       }
     }
   },
+  //слово делалим на массив букв
   getWordLetters() {
     return this.word.split('');
   },
+  //после 6 попыток проигрыш
   isGameOver() {
     return this.attempts === maxAttempts;
   },
+  //если не проигрыш, то победа и в разбитом на массивы букв слове есть совпадение с выбранной буквой
   isWin() {
     return !this.isGameOver() && this
       .getWordLetters().every((letter) => this.openedLetters.includes(letter));
   },
+  //конец игры, когда игра окончена , то буквы открываются 
   endGame() {
     this.openedLetters = alphabetLetters;
   },
 };
 
+//объект визуала игры, кнопки клавиатуры []
 const view = {
   keyboardButtons: [],
-
+//заголовок темы игры берется с помощью id в html, innerText извлекает текст
   renderTitle() {
     document.getElementById('topic').innerText = gameState.topic;
   },
 
+  //объект предпологаемого слова, возвращаем первый найденный элемент по селектору
   renderWord() {
+    //находим поле
     const container = document.querySelector('.word');
+    //отчищаем поле
     container.innerHTML = '';
+    //если буква совпадает , то она возвращается , если нет , то возвращается _
     const displayedWord = gameState.getWordLetters().map((letter) => {
       if (gameState.openedLetters.includes(letter)) {
         return letter;
       }
       return '_';
     });
+    // идем по загаданному слову
     for (const letter of displayedWord) {
+      //создаем текстовый контейнер, посредством создания html тэга?
       const placeholder = document.createElement('span');
+      //стилизация , навесили класс и положили нужный нам текст
       placeholder.className = 'letter';
       placeholder.innerText = letter;
+      //кладем по одной буквы 
       container.appendChild(placeholder);
     }
   },
-
+//объект клавиатуры
   renderKeyboard() {
+//берем элемент по id
     const keyboardContainer = document.getElementById('keyboard');
+    //выводим текст
     keyboardContainer.innerHTML = '';
+    //проходимся по массиву букв
     for (const letter of alphabetLetters) {
+      //создаем элемент "кнопка"
       const button = document.createElement('button');
+      //блокируем выбранные кнопки
       button.disabled = gameState.openedLetters.includes(letter);
+      //кладем в кнопку букву
       button.innerText = letter;
+      //добавляем кнопку
       keyboardContainer.appendChild(button);
-
+      //обраюотчик события. при клике отображаем буквы
       button.addEventListener('click', () => {
         gameState.openLetter(letter);
         this.render();
       });
     }
   },
-
+//выбираем контейнер по классу, устанавливаем атрибут, чтобы css подцепил атрибут и установил необходимые стили
   renderHangman() {
-  // implement this
-  // use gameState.attempts and maxAttempts
     document.querySelector('.hangman').setAttribute('data-step', gameState.attempts);
   },
-
+//выводим кнопку и делаем слово красным
   renderTryAgain() {
     document.getElementById('gameover').style.display = 'block';
     document.getElementById('word-guess').style.color = 'red';
   },
-
+//при победе делаем слово зеленым
   renderWin() {
-    document.getElementById('word-guess').style.color = 'green';
+    document.getElementById('win').style.display = 'block';
+    document.getElementById('word-guess2').style.color = 'green';
   },
 
+  //отрисовка всей страницы 
   render() {
     this.renderTitle();
     this.renderWord();
