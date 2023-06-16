39a82bdcc440a51f6821b219a16c2afd9a68ea06
IzarlyShark <115780892+IzarlyShark@users.noreply.github.com>
2023-06-16 23:05:21 +0300

    fix header and color word-guess

--- js/game.js
+++ js/game.js
@@ -26,7 +26,7 @@ const gameState = {
     this.openedLetters = [];
     this.topic = document.getElementById('topic').innerHTML;
     this.word = pickRandomWord(wordLists[this.topic]);
-    document.getElementById('word-guess').style.color = 'aqua';
+    document.getElementById('word-guess').style.color = 'rgba(244, 231, 156, 0.693)';
     document.querySelector('.hangman').style.opacity = '100%';
     /* for testing: */
     console.log(this.topic);
