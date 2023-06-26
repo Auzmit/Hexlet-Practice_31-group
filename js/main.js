349fe6bfc606ce1eb7be502f894d0e3553cb9505
Auzmit <atrudkov@mail.ru>
2023-06-26 19:17:09 +0300

    линтер фикс

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-const navBar = document.querySelector(".mobile-nav-bar");
-const primaryNav =document.querySelector(".primary-navigation");
+const navBar = document.querySelector('.mobile-nav-bar');
+const primaryNav = document.querySelector('.primary-navigation');
 
-navBar.addEventListener("click", () => {
-    primaryNav.hasAttribute("data-visible") ? navBar.setAttribute("aria-expanded", false) : navBar.setAttribute("aria-expanded", true);
-    primaryNav.toggleAttribute("data-visible");
+navBar.addEventListener('click', () => {
+  primaryNav.hasAttribute('data-visible') ? navBar.setAttribute('aria-expanded', false) : navBar.setAttribute('aria-expanded', true);
+  primaryNav.toggleAttribute('data-visible');
 });
-
+/* eslint-disable */
 const slider = new A11YSlider(document.querySelector('.slider'), {
-    adaptiveHeight: true,
-    dots: false
+  adaptiveHeight: true,
+  dots: false,
 });
+/* eslint-enable */
